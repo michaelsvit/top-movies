64863eeaf509302a55db5d00266ca06cae249adc
Michael Svit <michaelsvit@gmail.com>
2015-09-02 19:34:24 +0300

    Make Trailer and Review classes parcelable

--- app/src/main/java/com/example/michael/topmovies/Trailer.java
+++ app/src/main/java/com/example/michael/topmovies/Trailer.java
@@ -1,9 +1,12 @@
 package com.example.michael.topmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Michael on 9/2/2015.
  */
-public class Trailer {
+public class Trailer implements Parcelable {
     private String key;
     private String name;
 
@@ -12,11 +15,39 @@ public class Trailer {
         this.name = name;
     }
 
+    protected Trailer(Parcel in) {
+        key = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<Trailer> CREATOR = new Creator<Trailer>() {
+        @Override
+        public Trailer createFromParcel(Parcel in) {
+            return new Trailer(in);
+        }
+
+        @Override
+        public Trailer[] newArray(int size) {
+            return new Trailer[size];
+        }
+    };
+
     public String getKey() {
         return key;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(key);
+        dest.writeString(name);
     }
 }
